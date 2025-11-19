ebba8189d5dae1332910829859ef4c86bc850987
Thomas Ramel <132356535+Sevenlegarstropsympa@users.noreply.github.com>
2025-11-19 11:53:22 +0100

    movement + rotation

---
+++ Assets/Joueur.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Joueur : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public Camera cam;
+    public Vector2 movement;
+    public Vector2 mousePos;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Vector2 lookDir = mousePos - rb.position;
+
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
