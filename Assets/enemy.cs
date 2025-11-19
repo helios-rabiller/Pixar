31b521d84ff26e2539387df7c7452eb74be41c10
Thomas Ramel <132356535+Sevenlegarstropsympa@users.noreply.github.com>
2025-11-19 14:57:45 +0100

    Enemy + random spawner

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using System.Collections;  
+using System.Collections.Generic;
 
-public class enemy : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed;
+    public Transform player;
+    
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(player != null)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
